7b79711b417cd074257b12c72b96743d7ea86e40
gani155610100 <39738816+gani155610100@users.noreply.github.com>
2018-07-30 00:22:29 +0700

    Add files via upload

---
+++ admin/menu_admin.php
@@ -0,0 +1,72 @@
+       <div id="left">
+            <ul id="menu" class="collapse">
+                <li class="panel active"><a href="index.php"><i class="icon-home"></i> Dashboard</a></li>
+                <li><a href="?menu=pasien"><i class="icon-paper-clip"> </i> Pasien</a></li>
+                <li><a href="?menu=laborat"><i class="icon-paper-clip"></i> Laboratorium</a></li>
+                <li><a href="?menu=tindakan"><i class="icon-paper-clip"></i> Tindakan</a></li>
+                <li><a href="?menu=obat"><i class="icon-paper-clip"></i> Obat-obatan</a></li>
+                <li><a href="?menu=kunjungan"><i class="icon-paper-clip"></i> Kunjungan</a></li>
+                <li><a href="?menu=dokter"><i class="icon-paper-clip"></i> Dokter</a></li>
+                <li><a href="?menu=poliklinik"><i class="icon-paper-clip"></i> Polklinik</a></li>
+                <li><a href="?menu=rekam"><i class="icon-paper-clip"></i> Rekam Medis</a></li>
+                <li><a href="?menu=user"><i class="icon-user "></i> Daftar User</a></li>
+            </ul>
+        </div>
+		
+		
+		<div id="content">
+            <div class="inner">
+                <div class="row">
+                    <div class="col-lg-12">
+						<h1>Life Line Hospital</h1>
+                    </div>
+                </div>
+                <hr/>
+                 <!--BLOCK SECTION -->
+                 <div class="row">
+                    <div class="col-lg-12">
+						<?php
+						if($_GET["menu"]){
+							include_once("load.php");
+						}else{
+							echo "<div class='col-lg-12'>
+										<div class='panel panel-default'>
+											<div class='panel-heading'>
+												Tentang Aplikasi
+											</div>
+											<div class='panel-body'>
+												<ul class='nav nav-tabs'>
+													<li class='active'><a href='#home' data-toggle='tab'>Home</a>
+													</li>
+													<li><a href='#profile' data-toggle='tab'>Profil</a>
+													</li>
+													
+												</ul>
+
+												<div class='tab-content'>
+													<div class='tab-pane fade in active' id='home'>
+													<center>
+														<p><img src='../img/Picture2.png' class='img-responsive' alt='Header SIRS'/></p></center>
+													</div>
+													<div class='tab-pane fade' id='profile'>
+														<blockquote>
+															<table><tr><td align='left' width='200px'>
+															Nama Aplikasi</td><td>: SISTEM MANAJEMEN RUMAH SAKIT</td></tr>
+															<tr><td align='left'>Nama Pembuat</td><td>: DEWI IRAWATI ,RAHMI NANADA , MARIANA ELDA</td></tr>
+															
+															</table>
+														</blockquote>
+													</div>
+													
+												</div>
+											</div>
+										</div>
+									</div>";
+						}
+						?>
+					</div>
+                </div>
+                  <!--END BLOCK SECTION -->
+                <hr />
+            </div>
+        </div>
